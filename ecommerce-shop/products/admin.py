262cdf71e88ace542f2ce4a792efb43bc68c9a03
VINAYAKA J <vinayakaj29@gmail.com>
2026-02-09 23:45:28 +0530

    Update admin.py

--- ecommerce-shop/products/admin.py
+++ ecommerce-shop/products/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import Product, Order
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "created_at")
+    list_filter = ("created_at",)
     search_fields = ("name",)
+    ordering = ("-created_at",)
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("customer_name", "product", "quantity", "ordered_at")
+    list_display = (
+        "id",
+        "customer_name",
+        "customer_email",
+        "product",
+        "quantity",
+        "ordered_at",
+    )
+    list_filter = ("ordered_at",)
     search_fields = ("customer_name", "customer_email")
+    ordering = ("-ordered_at",)
